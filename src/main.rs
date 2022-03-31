da0df45b88aedf1c043b14c286baccc0028123ad
inFngNam <inf.ng.nam@gmail.com>
2022-04-01 00:53:12 +0700

    Menu Buttons

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowMode};
 
 mod plugins;
 
-const WINDOW_WIDTH: f32 = 960.0;
-const WINDOW_HEIGHT: f32 = 540.0;
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 1.0;
+pub const HEIGHT: f32 = 540.0;
+
+pub const MONITOR_WIDTH: f32 = 1920.0;
+pub const MONITOR_HEIGHT: f32 = 1080.0;
 
 fn main() {
-    let mut app = App::new();
-    app.insert_resource(window_desscriptor());
-    app.add_plugins(DefaultPlugins);
-    app.add_plugin(plugins::main_menu::MainMenuPlugin);
-    app.run();
-}
-
-fn window_desscriptor() -> WindowDescriptor {
-    return WindowDescriptor {
-        width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT,
-        title: "Shoot 'Em Up".to_string(),
-        vsync: true,
-        resizable: false,
-        mode: bevy::window::WindowMode::Windowed,
-        resize_constraints: bevy::window::WindowResizeConstraints {
-            min_width: WINDOW_WIDTH,
-            max_width: WINDOW_WIDTH,
-            min_height: WINDOW_HEIGHT,
-            max_height: WINDOW_HEIGHT,
-        },
-        ..Default::default()
-    };
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: HEIGHT * RESOLUTION * 10.0,
+            height: HEIGHT,
+            title: "Shoot 'em up".to_string(),
+            position: Some(Vec2::new(MONITOR_WIDTH / 4.0, MONITOR_HEIGHT / 4.0)),
+            vsync: true,
+            resizable: false,
+            resize_constraints: bevy::window::WindowResizeConstraints {
+                min_width: HEIGHT * RESOLUTION,
+                max_width: HEIGHT * RESOLUTION,
+                min_height: HEIGHT,
+                max_height: HEIGHT,
+                ..Default::default()
+            },
+            mode: WindowMode::Windowed,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system_to_stage(StartupStage::PreStartup, plugins::assets::load_ascii)
+        .add_plugin(plugins::main_menu::MainMenuPlugin)
+        .run();
 }
--- src/plugins/main_menu/mod.rs
+++ src/plugins/main_menu/mod.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
 
-const BUTTON_WIDTH: f32 = 175.0;
-const BUTTON_HEIGHT: f32 = 75.0;
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+const BUTTON_WIDTH: f32 = 250.0;
+const BUTTON_HEIGHT: f32 = 100.0;
 
 pub struct MainMenuPlugin;
 
@@ -9,8 +11,19 @@ impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         // app.add_event::<GameExitEvent>();
         // app.add_event::<StartTetrisEvent>();
+        // app.add_startup_system(spawn_camera);
         app.add_startup_system(setup);
     }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+    camera.orthographic_projection.right = 1.0 * RESOLUTION;
+    camera.orthographic_projection.left = -1.0 * RESOLUTION;
+    commands.spawn_bundle(camera);
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -21,28 +34,26 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                 ..Default::default()
             },
-            image: UiImage(asset_server.load("images/background.png")),
+            image: UiImage(asset_server.load("images/menu_background.png")),
             ..Default::default()
         })
         .with_children(|parent| {
             let spreate: f32 = BUTTON_HEIGHT / 4.0;
-
             // Position from right
-
             // Play Button
-            let play_button_position = Vec2::new(10.0, spreate);
+            let play_button_position = Vec2::new(20.0, spreate);
             parent.spawn_bundle(new_button(play_button_position, &asset_server));
 
             // Demos Button
-            let demos_button_position = Vec2::new(10.0, spreate * 2.0 + BUTTON_HEIGHT);
+            let demos_button_position = Vec2::new(20.0, spreate * 2.0 + BUTTON_HEIGHT);
             parent.spawn_bundle(new_button(demos_button_position, &asset_server));
 
             // Setting Button
-            let setting_button_position = Vec2::new(10.0, spreate * 3.0 + BUTTON_HEIGHT * 2.0);
+            let setting_button_position = Vec2::new(20.0, spreate * 3.0 + BUTTON_HEIGHT * 2.0);
             parent.spawn_bundle(new_button(setting_button_position, &asset_server));
 
             // Quit Button
-            let quit_button_position = Vec2::new(10.0, spreate * 4.0 + BUTTON_HEIGHT * 3.0);
+            let quit_button_position = Vec2::new(20.0, spreate * 4.0 + BUTTON_HEIGHT * 3.0);
             parent.spawn_bundle(new_button(quit_button_position, &asset_server));
         });
 }
@@ -65,7 +76,7 @@ fn new_button(position: Vec2, asset_server: &Res<AssetServer>) -> ButtonBundle {
             },
             ..Default::default()
         },
-        image: UiImage(asset_server.load("images/button.png")),
+        image: UiImage(asset_server.load("images/EmptyButton.png")),
         ..Default::default()
     }
 }
