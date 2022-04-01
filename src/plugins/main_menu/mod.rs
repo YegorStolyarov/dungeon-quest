9d6126ee44aed4036c7afe48afc539c7a31087c7
inFngNam <inf.ng.nam@gmail.com>
2022-04-01 07:16:04 +0700

    Text And Button Interact

--- src/plugins/main_menu/mod.rs
+++ src/plugins/main_menu/mod.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 
-const BUTTON_WIDTH: f32 = 250.0;
+const BUTTON_WIDTH: f32 = 300.0;
 const BUTTON_HEIGHT: f32 = 100.0;
 
 pub struct MainMenuPlugin;
@@ -13,6 +13,7 @@ impl Plugin for MainMenuPlugin {
         // app.add_event::<StartTetrisEvent>();
         // app.add_startup_system(spawn_camera);
         app.add_startup_system(setup);
+        app.add_system(button_system);
     }
 }
 
@@ -42,19 +43,35 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             // Position from right
             // Play Button
             let play_button_position = Vec2::new(20.0, spreate);
-            parent.spawn_bundle(new_button(play_button_position, &asset_server));
+            parent
+                .spawn_bundle(new_button(play_button_position, &asset_server))
+                .with_children(|parent| {
+                    parent.spawn_bundle(new_text("PLAY", &asset_server));
+                });
 
             // Demos Button
             let demos_button_position = Vec2::new(20.0, spreate * 2.0 + BUTTON_HEIGHT);
-            parent.spawn_bundle(new_button(demos_button_position, &asset_server));
+            parent
+                .spawn_bundle(new_button(demos_button_position, &asset_server))
+                .with_children(|parent| {
+                    parent.spawn_bundle(new_text("DEMOS", &asset_server));
+                });
 
             // Setting Button
             let setting_button_position = Vec2::new(20.0, spreate * 3.0 + BUTTON_HEIGHT * 2.0);
-            parent.spawn_bundle(new_button(setting_button_position, &asset_server));
+            parent
+                .spawn_bundle(new_button(setting_button_position, &asset_server))
+                .with_children(|parent| {
+                    parent.spawn_bundle(new_text("SETTING", &asset_server));
+                });
 
             // Quit Button
             let quit_button_position = Vec2::new(20.0, spreate * 4.0 + BUTTON_HEIGHT * 3.0);
-            parent.spawn_bundle(new_button(quit_button_position, &asset_server));
+            parent
+                .spawn_bundle(new_button(quit_button_position, &asset_server))
+                .with_children(|parent| {
+                    parent.spawn_bundle(new_text("QUIT", &asset_server));
+                });
         });
 }
 
@@ -67,6 +84,7 @@ fn new_button(position: Vec2, asset_server: &Res<AssetServer>) -> ButtonBundle {
             size: button_size,
             justify_content: JustifyContent::Center,
             position_type: PositionType::Absolute,
+            align_items: AlignItems::Center,
             align_self: AlignSelf::FlexEnd,
             position: Rect {
                 right: Val::Px(position.x),
@@ -78,5 +96,45 @@ fn new_button(position: Vec2, asset_server: &Res<AssetServer>) -> ButtonBundle {
         },
         image: UiImage(asset_server.load("images/EmptyButton.png")),
         ..Default::default()
+    }
+}
+
+fn new_text(value: &str, asset_server: &Res<AssetServer>) -> TextBundle {
+    TextBundle {
+        text: Text::with_section(
+            value,
+            TextStyle {
+                font: asset_server.load("fonts/Haedus.ttf"),
+                font_size: 80.0,
+                color: Color::WHITE,
+            },
+            TextAlignment {
+                vertical: VerticalAlign::Center,
+                horizontal: HorizontalAlign::Center,
+            },
+        ),
+        ..Default::default()
+    }
+}
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, children) in interaction_query.iter_mut() {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::None => {
+                text.sections[0].style.color = Color::WHITE.into();
+                *color = Color::WHITE.into();
+            }
+            _ => {
+                text.sections[0].style.color = Color::GREEN.into();
+                *color = Color::GREEN.into();
+            }
+        }
     }
 }
