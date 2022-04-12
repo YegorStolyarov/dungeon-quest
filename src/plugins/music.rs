974aca54eeac181b03527fc347697e0f130819d1
inFngNam <inf.ng.nam@gmail.com>
2022-04-13 00:59:52 +0700

    Update Background Music

--- src/plugins/music.rs
+++ src/plugins/music.rs
@@ -1,30 +1,47 @@
 use bevy::prelude::*;
-use bevy_kira_audio::{Audio, AudioChannel};
+use bevy_kira_audio::{Audio, AudioChannel, AudioSource};
 
 use crate::plugins::setting::Setting;
 
-pub fn start_background_music(
-    asset_server: Res<AssetServer>,
-    audio: Res<Audio>,
+pub struct BackgroundAudioChannel {
+    background_music: Handle<AudioSource>,
+    channel: AudioChannel,
+    loop_started: bool,
+    volume: f32,
+}
+
+pub fn init_background_audio_channel(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let background_audio_channel = BackgroundAudioChannel {
+        background_music: asset_server.load("sounds/background_music.ogg"),
+        channel: AudioChannel::new("background".to_owned()),
+        loop_started: false,
+        volume: 0.3,
+    };
+
+    commands.insert_resource(background_audio_channel);
+}
+
+pub fn play_background_music(
+    mut background_audio_channel: ResMut<BackgroundAudioChannel>,
     setting: Res<Setting>,
+    audio: Res<Audio>,
 ) {
-    let background_music = asset_server.load("sounds/background_music.ogg");
-    let background_channel = AudioChannel::new("background".to_owned());
-    audio.set_volume(0.15);
-    audio.set_volume_in_channel(0.15, &background_channel);
-    audio.play_looped_in_channel(background_music, &background_channel);
-
-    if setting.get_enable_music() == false {
-        stop_background_music(audio);
+    if setting.get_enable_music() {
+        if !background_audio_channel.loop_started {
+            background_audio_channel.loop_started = true;
+            audio.set_volume_in_channel(
+                background_audio_channel.volume,
+                &background_audio_channel.channel,
+            );
+            audio.play_looped_in_channel(
+                background_audio_channel.background_music.clone(),
+                &background_audio_channel.channel,
+            );
+        }
     } else {
-        resume_background_music(audio);
+        if background_audio_channel.loop_started {
+            background_audio_channel.loop_started = false;
+            audio.stop_channel(&background_audio_channel.channel);
+        }
     }
-}
-
-pub fn stop_background_music(audio: Res<Audio>) {
-    audio.stop();
-}
-
-pub fn resume_background_music(audio: Res<Audio>) {
-    audio.resume();
 }
