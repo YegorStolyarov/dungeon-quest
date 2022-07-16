98673bc29e84c8891822eaca13eef139ac26790c
inFngNam <inf.ng.nam@gmail.com>
2022-07-16 23:03:32 +0700

    Wall crops

---
+++ resources/scripts/wall_crop.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+from PIL import Image
+
+IMG_PATH = 'images/DungeonTilesetII.png'
+TILES_PATH = 'titles/wall_list.txt'
+
+def getBox(arr, frameIndex = 0):
+    w, h = int(arr[3]), int(arr[4])
+    x, y = int(arr[1]) + w*frameIndex, int(arr[2])
+    return (x, y, x+w, y+h)
+
+def saveCrop(img, title, box):
+    path = os.path.join('./results/walls/' + title + '.png')
+    try:
+        croppedImg = img.crop(box)
+        croppedImg.save(path)
+        print('ok: ' + title)
+    except Exception as e:
+        print('fail: ' + title + ' -- ' + str(e))
+
+img = Image.open(IMG_PATH)
+w, h = img.size
+
+f = open(TILES_PATH, 'r')
+for line in f.readlines():
+    arr = line.split()
+    if len(arr) == 0:
+        pass
+    if len(arr) == 5:
+        saveCrop(img, arr[0], getBox(arr))
+    if len(arr) == 6:
+        numOfFrames = int(arr[5])
+        for frameIndex in range(0, numOfFrames):
+            saveCrop(img, arr[0] + '_f' + str(frameIndex), getBox(arr, frameIndex))
+
+print('')
+
+# elf_f_idle_anim 128 4 16 28 4
